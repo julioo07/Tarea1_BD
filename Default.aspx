7851f86443fd3de86b6383e956882d730909fe04
Fabricio Picado <fabripicado@gmail.com>
2024-02-25 17:25:32 -0600

    upload first

---
+++ Default.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TareaBD._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <main>
+
+        <div class="row">
+            <asp:Panel runat ="server" ID="pnlDatosEmpleado">
+
+                Lista de Empleados
+                <br/>
+                <br/>
+
+        <asp:GridView ID="gvdEmpleados" runat="server" AutoGenerateColumns="true">
+
+
+        </asp:GridView>
+                <br/>
+                <br/>
+                <asp:button ID="btnInsertarEmpleado" Text="Insertar Empleado" runat="server" OnClick="btnInsertarEmpleado_Click" />
+            </asp:Panel>
+
+
+            <asp:Panel ID="pnlAltaEmpleado" runat="server" Visible="false">
+                <div> 
+                    <br/>
+                    <asp:Label ID="lblNombre" Text="Nombre: " runat="server"></asp:Label>
+                    <asp:TextBox ID="txtNombre" runat="server" />
+                </div>
+
+                <div>
+                    <br/>
+                    <asp:Label ID="Salario" Text="Salario:" runat="server"></asp:Label>
+                    <asp:TextBox ID="txtSalario" runat="server" />
+                </div>
+                <br/>
+                <br/>
+                    <asp:button ID="btnGuardar" runat="server" Text="Insertar" OnClick="btnGuardar_Click" />
+                    <asp:Button ID="btnRegresar" runat="server" Text="Regresar" OnClick="btnRegresar_Click" />
+            </asp:Panel>
+
+            <br/>
+            
+
+
+        </div>
+    </main>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using static System.Net.Mime.MediaTypeNames;
+
+
+
+namespace TareaBD
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargaDatosEmpleado();
+            }
+        }
+        public void CargaDatosEmpleado()
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connDB"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SPSEmpleados";  // Nombre del stored procedure encargado de EnseñarEmpleados
+                cmd.Connection = conn;
+                conn.Open();
+                gvdEmpleados.DataSource = cmd.ExecuteReader();
+                gvdEmpleados.DataBind();
+            }
+        }
+
+        public void InsertarEmpleado()
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connDB"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SPIEmpleado";  // nombre del stored procedure encargado de InsertEmpleados
+                cmd.Parameters.Add("@Nombre", SqlDbType.VarChar).Value = txtNombre.Text.Trim();
+                cmd.Parameters.Add("@Salario", SqlDbType.Money).Value = decimal.Parse(txtSalario.Text.Trim());
+                cmd.Connection = conn;
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+
+        protected void btnInsertarEmpleado_Click(object sender, EventArgs e)
+        {
+            pnlDatosEmpleado.Visible = false;
+            pnlAltaEmpleado.Visible=true;
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            pnlAltaEmpleado.Visible=false;
+            pnlDatosEmpleado.Visible=true;
+            
+            //lblMensaje.Text = "Inserción exitosa";
+            //lblMensaje.Visible = true;
+            InsertarEmpleado();
+            CargaDatosEmpleado();
+        }
+
+        protected void btnRegresar_Click(object sender, EventArgs e)
+        {
+            pnlAltaEmpleado.Visible=false;
+            pnlDatosEmpleado.Visible=true;
+        }
+    }
+}
